c71626fb53ec5c2542f8e34fb0e0555a141a0fe2
Lee Briggs <coffeegunshow@gmail.com>
2021-02-18 13:36:30 +1300

    feat: add i18n adapter and use in controller

--- typescript/structural/adapter/controllers/NewsController.ts
+++ typescript/structural/adapter/controllers/NewsController.ts
@@ -3,12 +3,28 @@ import InternationalNews from '../services/InternationalNews'
 import renderNewsItems from '../views/newsItems'
 import { render } from '../helpers/rendering'
 
+// TODO: use generic type in place of LocalNews everywhere
+// type News = {
+//   headline: string;
+//   published_at: string;
+//   tagline: string;
+// }
+
+type InternationalNewsAll = { all: () => InternationalNews[]}
+class InternationalNewsAdapter {
+  constructor(private service: InternationalNewsAll) {}
+
+  all(): LocalNews[] {
+    return this.service.all().map(n => new LocalNews(n.title, n.date, n.description));
+  }
+}
 class NewsController {
   index() {
     render('Fetching articles...')
-    const newsItems: (LocalNews | InternationalNews)[] = []
+    const newsItems: LocalNews[] = []
     const localNewsItems = LocalNews.all()
-    const internationalNewsItems = InternationalNews.all()
+    // Use our adapter
+    const internationalNewsItems = (new InternationalNewsAdapter(InternationalNews)).all()
 
     newsItems.push(...internationalNewsItems)
     newsItems.push(...localNewsItems)
