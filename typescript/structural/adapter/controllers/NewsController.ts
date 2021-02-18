128390c84f0b1eb57cf0c2e5fae9e824f62876cd
Cam Stark <camstark@CamStarksPuter.modem>
2021-02-18 11:09:28 +1100

    Answer 2: Create Adapter Class for IntNews

--- typescript/structural/adapter/controllers/NewsController.ts
+++ typescript/structural/adapter/controllers/NewsController.ts
@@ -2,9 +2,17 @@ import LocalNews from '../models/LocalNews'
 import InternationalNews from '../services/InternationalNews'
 import renderNewsItems from '../views/newsItems'
 import { render } from '../helpers/rendering'
+import ViewableNews from '../interface/ViewableNews'
 
-function adaptTheNews(internationalNews: InternationalNews) {
-  return new LocalNews(internationalNews.title, internationalNews.date, internationalNews.description)
+class InternationalNewsAdapter implements ViewableNews {
+  constructor(private news: InternationalNews) {}
+  get headline(): string { return this.news.title }
+  get published_at(): Date { return this.news.date }
+  get tagline(): string { return this.news.description }
+}
+
+function adaptTheNews(internationalNews: InternationalNews): ViewableNews {
+  return new InternationalNewsAdapter(internationalNews);
 }
 
 class NewsController {
